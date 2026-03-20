a5dafcd3e89f3b0d71617e0018f39df7175de9a4
u9602041 <u9602041@gmail.com>
2026-03-20 09:37:38 +0000

    準備發布上架

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// @ts-check
-
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
+// 這是告訴 Astro 你的正式網址是什麼
+// 這樣未來產生 sitemap 或文章網址時才不會出錯
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: 'https://smilecloud.tw',
 });
