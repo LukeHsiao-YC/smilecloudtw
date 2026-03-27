5034601d2fb375dde51a27629453cbd15045d3c0
u9602041 <u9602041@gmail.com>
2026-03-27 09:29:49 +0000

    terminal

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // 這是告訴 Astro 你的正式網址是什麼
 // 這樣未來產生 sitemap 或文章網址時才不會出錯
 export default defineConfig({
   site: 'https://smilecloud.tw',
+  integrations: [sitemap()],
 });
