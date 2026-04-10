4f10db9228d0d27726abd96e2ae4fe7c27220595
u9602041 <u9602041@gmail.com>
2026-04-10 02:55:11 +0000

    C

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,7 @@
 import { defineCollection, z } from 'astro:content';
-// 匯入最新版系統專用的抓取器
-import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-    // 加上 loader 讓最新版 Astro 找得到衛教文章
-    loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+    type: 'content',
     schema: ({ image }) => z.object({
         title: z.string(),
         description: z.string().optional(),
@@ -20,8 +17,7 @@ const blog = defineCollection({
 });
 
 const guideline = defineCollection({
-    // 關鍵！必須告訴 Astro 去這個資料夾抓 .md 檔
-    loader: glob({ base: './src/content/guideline', pattern: '**/*.{md,mdx}' }),
+    type: 'content',
     schema: z.object({
         title: z.string().optional(),
     }),
